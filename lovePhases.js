3314ff2e020630e303dfba7039094d451411181e
ThiagoGarcia <thiagoncg1@gmail.com>
2022-08-08 17:13:33 -0300

    Variaveis
    Criação de variaveis let, const,  var.

---
+++ lovePhases.js
@@ -0,0 +1,15 @@
+console.log(
+    [{
+        phase: "O verdadeiro amor nunca se desgasta. Quando mais se dá, mais se tem.",
+        autor: "Antoine de Saint-Exupéry"
+    },
+    {
+        phase: "Eu te amo é uma frase muito forte para ser dita em vão. Ame primeiro, diga depois.",
+        autor: "Renato Russo",
+    },
+    {
+        phase: "Se o amor é fantasia, eu me encontro ultimamente em pleno carnaval.",
+        autor: "Toquinho e Mutinho",
+    }]
+);
+
