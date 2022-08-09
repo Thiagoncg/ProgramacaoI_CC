eae268eab50e97a13497fb287e822663162195d4
Thiago Garcia <thiagoncg1@gmail.com>
2022-08-09 20:47:53 -0300

    functions
    functions

--- functions/functionss.js
+++ functions/functionss.js
@@ -25,15 +25,67 @@
 // summ(0, 3);
 
 //---------------------------------------------------
-const summm = function(number1, number2) {
-    total = number1 + number2;
-    return total
-}
+// const summm = function(number1, number2) {
+//     total = number1 + number2;
+//     return total
+// }
 
-let number1 = 5;
-let number2 = 5;
-console.log(`o numero 1 é ${number1}`);
-console.log(`o numero 2 é ${number2}`);
-console.log(`a soma é  ${summm(number1, number2)}`);
-// OU
-console.log(`A soma é`, total);
+// let number1 = 5;
+// let number2 = 5;
+// console.log(`o numero 1 é ${number1}`);
+// console.log(`o numero 2 é ${number2}`);
+// console.log(`a soma é  ${summm(number1, number2)}`);
+// // OU
+// console.log(`A soma é`, total);
+
+//--------------------------------------------------------------------
+
+// let total = 0;
+// const summm = function(number1, number2) {
+//     let total = number1 + number1;
+//     return total
+// }
+
+// let number1 = 10;
+// let number2 = 5;
+// console.log(`o numero 1 é ${number1}`);
+// console.log(`o numero 2 é ${number2}`);
+// console.log(`a soma é  ${summm(number1, number2)}`);
+// // OU
+// console.log(`A soma é`, total); // retornou 0 devido ao valor fora do escopo
+
+
+//Function Scope
+// let object = 'computer'
+// function createObject() { // Sem parametro
+//     object = 'table';
+//     return object
+// }
+// console.log(createObject(object));
+// console.log(object);
+//-------------------------------------------------------
+// let object = 'computer'
+// function createObject(object) { // Com parametro
+//     object = 'table';
+//     return object
+// }
+// console.log(createObject(object));
+// console.log(object);
+
+
+
+
+//Função hosting
+// sayName()
+
+// function sayName(){
+// console.log('Thiago Garcia')
+// }
+
+// Este tipo de função sofre elevação de forma automática
+
+// sayName()
+
+// const sayName = function(){
+// console.log('Thiago Garcia')
+// } // erro não sofre elevação, ao atribuir um valor de variável
