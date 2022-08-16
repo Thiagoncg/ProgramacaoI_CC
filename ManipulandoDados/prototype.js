dfb98efa404f8e729923520792bd5ec0da38ae91
ThiagoGarcia <thiagoncg1@gmail.com>
2022-08-16 16:39:27 -0300

    Manipulando dados
    Manipulando dados

---
+++ ManipulandoDados/prototype.js
@@ -0,0 +1,38 @@
+/*
+    Prototype
+
+    *prototype-based language
+    *prototype chain
+    *__proto__
+*/
+// no cosole web
+// Tranformar string em numero e numeros em string.
+//No console web
+//Converte string em numero
+let string = "500";
+
+let totalString = string + 500;
+console.log(totalString);
+
+totalString  = Number(string) + 500;
+console.log(totalString);
+
+
+//Converte numero em string
+let number = 100;
+
+let totalNumber = number + 100;
+console.log(totalNumber);
+
+totalNumber = String(number) + 100;
+console.log(totalNumber);
+
+//Conta quantos caracteres tem uma palavra, conta quantos digitos de um numero;
+let palavra = "Paralelepipedo";
+console.log(palavra.length);
+
+let numero = 123456789123456789;
+console.log(String(numero).length);
+
+
+
